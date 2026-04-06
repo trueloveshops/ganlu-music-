e476bbdd24e04dcc0ecec5107bbca7016b5d8f1f
trueloveshops <trueloveshops@gmail.com>
2026-04-07 00:20:37 +0800

    update site
    update site

---
+++ quotes.js
@@ -0,0 +1,17 @@
+const QUOTES = [
+  {
+    content: "當我們內在平靜，世界也會變得平靜。",
+    date: "清海無上師以英文講於1996.7.19",
+    url: "https://suprememastertv.com/ch1/max/"
+  },
+  {
+    content: "愛是最強大的力量，可以改變一切。",
+    date: "清海無上師開示",
+    url: "https://suprememastertv.com/ch1/max/"
+  },
+  {
+    content: "真正的智慧來自內在的覺醒。",
+    date: "清海無上師",
+    url: "https://suprememastertv.com/ch1/max/"
+  }
+];
